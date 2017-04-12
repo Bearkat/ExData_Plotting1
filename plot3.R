f4fbff8c1c29031dc5de068bf016c6629aaf8d83
Bearkat <dbarakat8@gmail.com>
2017-04-12 13:38:57 -0400

    plots and code added

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-##ensure that the file "household_power_consumption.txt" is in you working directory. 
+##ensure that the file "household_power_consumption.txt" is in your working directory. 
 power_data <- read.table(file = "household_power_consumption.txt", 
                          skip = grep("1/2/2007", readLines("household_power_consumption.txt"))[1], 
                          nrows = 2879, stringsAsFactors = FALSE, sep = ";", 
@@ -12,13 +12,12 @@ for(i in 1:length(power_data[,1])){dt[i] <- paste(power_data[i, 1],
 
 dt1 <- dmy_hms(dt)
 
-png(filename = "plot4.png")
-par(mfrow = c(2, 2))
-plot(dt1, power_data$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "", type = "l");
-plot(dt1, power_data$Voltage, ylab = "Voltage", xlab = "", type = "l");
-plot(dt1, power_data$Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "n");
-lines(dt1, power_data$Sub_metering_1, col = "black");
-lines(dt1, power_data$Sub_metering_2, col = "red");
-lines(dt1, power_data$Sub_metering_3, col = "blue");
-plot(dt1, power_data$Global_reactive_power, ylab = "Global_reactive_power", xlab = "", type = "l");
+png(filename = "plot3.png")
+par(mar = c(5, 4, 4, 4))
+plot(dt1, power_data$Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "n")
+lines(dt1, power_data$Sub_metering_1, col = "black")
+lines(dt1, power_data$Sub_metering_2, col = "red")
+lines(dt1, power_data$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), lty = c(1, 1, 1), 
+       legend = c("sub_metering_1", "sub_metering_2", "sub_metering_3"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -12,11 +12,15 @@ for(i in 1:length(power_data[,1])){dt[i] <- paste(power_data[i, 1],
 
 dt1 <- dmy_hms(dt)
 
-png(filename = "plot3.png")
-
-plot(dt1, power_data$Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "n")
-lines(dt1, power_data$Sub_metering_1, col = "black")
-lines(dt1, power_data$Sub_metering_2, col = "red")
-lines(dt1, power_data$Sub_metering_3, col = "blue")
-
+png(filename = "plot4.png")
+par(mfrow = c(2, 2))
+plot(dt1, power_data$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "", type = "l");
+plot(dt1, power_data$Voltage, ylab = "Voltage", xlab = "", type = "l");
+plot(dt1, power_data$Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "n");
+lines(dt1, power_data$Sub_metering_1, col = "black");
+lines(dt1, power_data$Sub_metering_2, col = "red");
+lines(dt1, power_data$Sub_metering_3, col = "blue");
+legend("topright", col = c("black", "red", "blue"), lty = c(1, 1, 1), 
+       legend = c("sub_metering_1", "sub_metering_2", "sub_metering_3"), cex = 0.7);
+plot(dt1, power_data$Global_reactive_power, ylab = "Global_reactive_power", xlab = "", type = "l");
 dev.off()
